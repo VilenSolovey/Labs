e23617719a848c49bafd3cf7c2c77a746ac7dc7e
VilenSolovey <solovey.vilen21@gmail.com>
2024-02-27 13:45:54 +0200

    Update Test_Lab1

--- Test_Lab1.py
+++ Test_Lab1.py
@@ -18,6 +18,15 @@ class TestLab1(unittest.TestCase):
     def test_null_array(self):
         self.assertTrue(is_arr_monotonic([]))
 
+    def test_for_correctness1(self):
+        self.assertTrue(is_arr_monotonic([2, 2, 2, 2, 1]))
+
+    def test_for_correctness2(self):
+        self.assertTrue(is_arr_monotonic([3, 3, 3, 3, 3]))
+
+    def test_for_correctness3(self):
+        self.assertTrue(is_arr_monotonic([3, 3, 3, 3, 5]))
+
 
 if __name__ == '__main__':
     unittest.main()
